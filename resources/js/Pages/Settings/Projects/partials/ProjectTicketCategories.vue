9d1cd88c18892303eab2f9461d20b0e3fc73a19b
Aurélien Chappard <aurelien.chappard@deefuse.fr>
2025-02-19 16:52:19 +0100

    Start create categorie in project create page

---
+++ resources/js/Pages/Settings/Projects/partials/ProjectTicketCategories.vue
@@ -0,0 +1,42 @@
+<template>
+<Card card-title="Catégorie des tickets">
+    <p class="text-secondary">Les catégories de ticket sont un moyen supplémentaire de trier ces derniers. Vous pouvez créer autant de catégorie que vous le souhaitez.</p>
+    <pre>{{form.ticket_categories}}</pre>
+    <pre>{{newCategorieName}}</pre>
+    <template #cardFooter>
+        <div class="row g-3">
+            <div class="col-sm-3">
+                <TextInput v-model.trim="newCategorieName" @keydown.enter.prevent="pushCategory" />
+            </div>
+            <div class="col-sm">
+                <PrimaryButton type="button" @click.prevent="pushCategory" :disabled="newCategorieName===''" :outlined="true">Ajouter la catégorie</PrimaryButton>
+            </div>
+        </div>
+
+
+    </template>
+</Card>
+</template>
+
+<script setup>
+import Card from "@/Components/ui/Card.vue";
+import TextInput from "@/Components/ui/form/TextInput.vue";
+import PrimaryButton from "@/Components/ui/form/PrimaryButton.vue";
+import {ref} from "vue";
+
+const newCategorieName = ref("");
+const props = defineProps({
+    form: {
+        type: Object,
+        required: true
+    }
+})
+
+const pushCategory = () => {
+    if(newCategorieName.value !== ''){
+        props.form.ticket_categories.push(newCategorieName.value);
+        newCategorieName.value = '';
+    }
+
+}
+</script>
